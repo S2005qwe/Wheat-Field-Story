d8b270702dfe9c23a2971486eaf2b5e7fc60a79c
S2005qwe <2919574867@qq.com>
2025-11-12 20:32:01 +0800

    初始化背包数据

---
+++ Assets/Scripts/Utilities/DataCollection.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ItemDetails
+{
+    public int id; //id
+
+    public string name;//名字
+
+    public ItemType itemType;//类型
+
+    public Sprite itemIcon; //图标
+
+    public Sprite itemOnWorldSprite; //边框
+
+    public string itemDescription;  //描述
+
+    public int itemUseRadius; //使用范围
+
+    public bool canPickedUp; //拾取
+
+    public bool canDropped;  //扔下
+
+    public bool canCarried; //举起
+
+    public int itemPrice;
+    [Range(0,1)]
+    public float sellPercentage;
+}
---
+++ Assets/Scripts/Utilities/Enums.cs
@@ -0,0 +1,10 @@
+public enum ItemType
+{ 
+    Seed,Commondity,Furniture,
+
+   //锄头   砍树工具  砸石头工具 割草工具  浇水工具  收割工具
+    HoeTool,ChopTool,BreakTool,ReapTool,WaterTool,CollectTool,
+
+    //杂草
+    ReapableScenery
+}
