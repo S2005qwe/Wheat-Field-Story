ccc6c91c0999979721a4c3a081557c807fe3e930
S2005qwe <2919574867@qq.com>
2025-11-22 20:34:45 +0800

    构建游戏的时间系统和时间系统UI制作

---
+++ Assets/Scripts/Time/Logic/TimeManager.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    private int gameSecond, gameMinute, gameHour, gameDay, gameMonth, gameYear;
+
+    private Season gameSeason = Season.夏天;
+
+    private int monthInSeason = 3;
+
+    public bool gameClockPause;
+
+    private float tikTime;
+
+    private void Awake()
+    {
+        NewGameTime();
+    }
+
+    private void Update()
+    {
+        if (!gameClockPause)
+        {
+            tikTime += Time.deltaTime;
+            if (tikTime >= Settings.secondThreshold)
+            {
+                tikTime -= Settings.secondThreshold;
+                UpdateGameTime();
+            }
+        }
+    }
+
+    //初始化
+    private void NewGameTime()
+    {
+        gameSeason = 0;
+        gameMinute = 0;
+        gameHour = 7;
+        gameDay = 1;
+        gameMonth = 1;
+        gameYear = 2022;
+        gameSeason = Season.春天;
+    }
+
+    //更新时间
+    public void UpdateGameTime()//更新时间
+    {
+        gameSecond++;
+        if (gameSecond > Settings.secondHold)
+        {
+            gameMinute++;
+            gameSecond = 0;
+
+            if (gameMinute > Settings.minuteHold)
+            {
+                gameHour++;
+                gameMinute = 0;
+
+                if (gameHour > Settings.hourHold)
+                {
+                    gameDay++;
+                    gameHour = 0;
+
+                    if (gameDay > Settings.dayHold)
+                    {
+                        gameDay = 1;
+                        gameMonth++;
+
+                        if (gameMonth > 12)
+                        {
+                            gameMonth = 1;
+                        }
+
+                        monthInSeason--;
+                        if (monthInSeason == 0)
+                        {
+                            monthInSeason = 3;
+
+                            int seasonNumber = (int)gameSeason;
+                            seasonNumber++;
+
+                            if (seasonNumber > Settings.seasonHold)
+                            {
+                                seasonNumber = 0;
+                                gameYear++;
+                            }
+
+                            gameSeason = (Season)seasonNumber;
+
+                            if (gameYear > 9999)
+                            {
+                                gameYear = 2022;
+                            }
+                        }
+                    }
+
+
+                }
+
+            }
+
+        }
+
+        Debug.Log("Second:" + gameSecond + "Minute:" + gameMinute);
+    }
+}
--- Assets/Scripts/Utilities/Settings.cs
+++ Assets/Scripts/Utilities/Settings.cs
@@ -11,4 +11,17 @@ public class Settings
 
     public const float targetAlpha = 0.45f;
 
+    //时间相关
+    public const float secondThreshold = 0.1f; //数值越小时间越快
+
+    public const int secondHold = 59;
+
+    public const int minuteHold = 59;
+
+    public const int hourHold = 23;
+
+    public const int dayHold = 10;
+
+    public const int seasonHold = 3;
+
 }
